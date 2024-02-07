8ef588b24fdd7b1245286c742743149158f7c972
Shahriar Shatil <52494840+ShatilKhan@users.noreply.github.com>
2024-02-08 01:57:24 +0600

    Time: 54 ms (54.98%) | Memory: 49 MB (20.35%) - LeetSync

---
+++ 2337-remove-digit-from-number-to-maximize-result/remove-digit-from-number-to-maximize-result.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string} number
+ * @param {character} digit
+ * @return {string}
+ */
+var removeDigit = function(number, digit) {
+    let last ;
+
+    for( let i = 0; i < number.length ; i++) {
+        if(number[i] !== digit) continue;
+        last = i;
+        if(number[ i + 1 ] <= digit) continue;
+        return number.slice(0,i) + number.slice(i+1);
+    }
+    return number.slice(0,last) + number.slice(last + 1);
+};
